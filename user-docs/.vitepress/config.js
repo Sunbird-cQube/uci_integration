0b652aa7a3a428eede6f1b0cfc75e095616c3144
Himanshu-AT <aristech.official@gmail.com>
2024-02-10 14:14:06 +0530

    generate demo docs

---
+++ user-docs/.vitepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    title: "UCI Integration EndUserDocs",
+    description: "UCI Integration EndUserDocs",
+    themeConfig: {
+        nav: [
+            { text: "Home", link: "/" },
+            { text: "initial github", link: "https://github.com/Sunbird-cQube/uci_integration.git" }
+        ],
+        sidebar: [
+            {
+                text: "Getting Started",
+                items: [
+                    { text: "Get Started", link: "/" },
+                    { text: "UseCase", link: "/usecase" },
+                ]
+            }
+        ]
+    }
+};
